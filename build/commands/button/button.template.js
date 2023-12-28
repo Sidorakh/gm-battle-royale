0e700605008abe9b3691e6ec0eaa525ea3de18a2
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2023-12-28 11:12:35 +1100

    Cleaning up the repo

--- build/commands/button/button.template.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.command = void 0;
-exports.command = {
-    id: 'button',
-    async handler(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-    }
-};
